9abc9963bc1a04c94442e596b8262282920641db
Daniel Hathorn <dmhathorn@gmail.com>
2011-01-31 23:18:15 -0700

    C1

--- project02/app/controllers/store_controller.rb
+++ project02/app/controllers/store_controller.rb
@@ -2,5 +2,16 @@ class StoreController < ApplicationController
   def index
 	  @products = Product.find_products_for_sale
   end
-
+  def add_to_cart
+	  product = Product.find(params[:id])
+	  @cart = find_cart
+	  @cart.add_product(product)
+  end
+  private
+  def find_cart
+	  unless session[:cart]
+		 session[:cart] = Cart.new
+	  end
+	  session[:cart]
+  end
 end
