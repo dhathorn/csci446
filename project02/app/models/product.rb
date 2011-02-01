481bcaff182914139e4c6c1ff249fa1b5f3f36b4
Daniel Hathorn <dmhathorn@gmail.com>
2011-01-31 21:04:16 -0700

    B1

--- project02/app/models/product.rb
+++ project02/app/models/product.rb
@@ -6,4 +6,7 @@ class Product < ActiveRecord::Base
 	def price_must_be_at_least_a_cent
 		errors.add(:price, 'should be at least 0.01') if price.nil? || price < 0.01
 	end
+	def self.find_products_for_sale
+		find(:all, :order => "title")
+	end
 end
