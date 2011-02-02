505abea7a0950d44d418eed2c31daa9f04dcc0a9
Daniel Hathorn <dmhathorn@gmail.com>
2011-02-02 02:56:26 -0700

    I18n

--- project02/app/controllers/application_controller.rb
+++ project02/app/controllers/application_controller.rb
@@ -4,17 +4,34 @@
 class ApplicationController < ActionController::Base
   layout "store"
   before_filter :authorize, :except => :login
+  before_filter :set_locale
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
   protected
-  def authorize
+   def authorize
 	  unless User.find_by_id(session[:user_id])
 		  flash[:notice] = "Please log in"
 		  redirect_to :controller => 'admin', :action => 'login'
 	  end
+  end
+   def set_locale
+	  session[:locale] = params[:locale] if params[:locale]
+	  I18n.locale = session[:locale] || I18n.default_locale
+	  locale_path = "#{LOCALES_DIRECTORY}#{I18n.locale}.yml"
+	  
+	  unless I18n.load_path.include? locale_path
+		  I18n.load_path << locale_path
+		  I18n.backend.send(:init_translation)
+	  end
+
+  rescue Exception => err
+	  logger.error err
+	  flash.now[:notice] = "#{I18n.locale} translation not available"
+	  I18n.load_path -= [locale_path]
+	  I18n.locale = session[:locale] = I18n.default_locale
   end
 
 
---
+++ project02/config/initializers/i18n.rb
@@ -0,0 +1,5 @@
+I18n.default_locale = 'en'
+
+LOCALES_DIRECTORY = "#{RAILS_ROOT}/config/locales/"
+LANGUAGES = { 'English' => 'en', "Russian" => 'ru',  "Espa\xc3\xb1ol" => 'es' }
+
