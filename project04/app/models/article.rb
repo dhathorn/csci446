4764f958a43c60853df6a07edeeab32ebfdfc91b
Daniel Hathorn <dmhathorn@gmail.com>
2011-02-05 19:28:17 -0700

    Basic funtionality

---
+++ project04/app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ActiveRecord::Base
+	validates_presence_of :title, :body, :author
+	validate :no_sally
+
+	protected
+
+	def no_sally
+		errors.add(:author, 'cannot be named Sally') if author =~ /\bSally\b/i 
+	end
+end
