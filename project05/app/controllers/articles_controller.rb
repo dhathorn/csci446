277055b63bc1fc1d8625dd108a4460eebf76a414
Daniel Hathorn <dmhathorn@gmail.com>
2011-02-15 19:36:47 -0700

    got rid of the xml stuff

--- project05/app/controllers/articles_controller.rb
+++ project05/app/controllers/articles_controller.rb
@@ -3,33 +3,18 @@ class ArticlesController < ApplicationController
   # GET /articles.xml
   def index
     @articles = Article.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @articles }
-    end
   end
 
   # GET /articles/1
   # GET /articles/1.xml
   def show
     @article = Article.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @article }
-    end
   end
 
   # GET /articles/new
   # GET /articles/new.xml
   def new
     @article = Article.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @article }
-    end
   end
 
   # GET /articles/1/edit
@@ -43,10 +28,8 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params[:article])
     @article.edit_count = 0
-
 		if @article.save
-			format.html { redirect_to(@article, :flash => {:success => 'Article was successfully created.'}) }
-			format.xml  { render :xml => @article, :status => :created, :location => @article }
+			redirect_to(@article, :flash => {:success => 'Article was successfully created.'})
 		else
 			flash[:error] = 'Error creating article.'
 			render :action => "new"
@@ -70,8 +53,6 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-
-		format.html { redirect_to(articles_url) }
-		format.xml  { head :ok }
+		redirect_to(articles_url)
   end
 end
