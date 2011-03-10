471b1c4f98df5481ce36c77d58ab2e26cd53c753
Daniel Hathorn <dmhathorn@gmail.com>
2011-03-10 00:30:50 -0700

    nicedly displayed user photos

--- project06/app/helpers/application_helper.rb
+++ project06/app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def display_photo(user)
+    if(user.photo.file?) then image_tag user.photo.url end
+  end
+
 end
+
